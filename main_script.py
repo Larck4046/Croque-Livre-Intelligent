f3c4cf7d636125180fadd85254ce179d7968b203
Larck4046 <marek.filion@gmail.com>
2026-01-12 09:28:24 -0500

    A lot of changes

--- main_script.py
+++ main_script.py
@@ -1,17 +1,15 @@
-import data_base
 from flask import Flask, render_template
-name_server = 'Script_html'
-site = Flask(name_server)
+
+
+site = Flask(__name__)
 
 @site.route('/')
 def index():
-    return render_template('web_gui/index.html')
+    return render_template("index.html")
 
 @site.route('/execute_script')
 def execute_script():
-    # Your Python script logic here
-    result = "Python script executed!"
-    return result
+    return "Python script executed!"
 
 if __name__ == '__main__':
     site.run(debug=True, host='0.0.0.0')
